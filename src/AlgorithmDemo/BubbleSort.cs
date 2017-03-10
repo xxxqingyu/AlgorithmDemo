39709b1b92cb2bc274f67057034783c0c5633f54
unknown <xuxuanxuan@vcredit.cn>
2017-03-10 09:56:22 +0800

    Add BubbleSort

---
+++ src/AlgorithmDemo/BubbleSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlgorithmDemo
+{
+    /// <summary>
+    /// 最差时间分析:O(n2)
+    /// 平均时间复杂度:O(n2)
+    /// 稳定度:稳定
+    /// 空间复杂度:O(1)
+    /// </summary>
+    public class BubbleSort
+    {
+        public void Sort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 1; j < array.Length - i; j++)
+                {
+                    if (array[j - 1] > array[j])
+                    {
+                        Tools.Swap(array, j - 1, j);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/AlgorithmDemo/Tools.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlgorithmDemo
+{
+    public class Tools
+    {
+        public static void Swap(int[] array, int i, int j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+
+        public static int[] GetInputArray()
+        {
+            Console.Write("请输入数组:");
+            var items = Console.ReadLine().Split(",， ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            return items.Select(n => int.Parse(n)).ToArray();
+        }
+
+        public static int[] CopeArray(int[] array)
+        {
+            int[] bArray = new int[array.Length];
+            Array.Copy(array, bArray, array.Length);
+            return bArray;
+        }
+
+        public static void Print(int[] array)
+        {
+            Console.Write("数组：");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(" {0,3} ", array[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
